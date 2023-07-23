714aa48d86387c252f4eb57c4d415902d5c6118b
FertileFragrance <1016030318@qq.com>
2023-07-23 13:00:54 +0800

    fix: remove gc

--- src/main/java/checker/OneOffChecker.java
+++ src/main/java/checker/OneOffChecker.java
@@ -11,7 +11,6 @@ import violation.Violation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Objects;
 
 public class OneOffChecker {
@@ -67,27 +66,5 @@ public class OneOffChecker {
             currentTxn.setExtWriteKeys(extWriteKeys);
         }
         return violations;
-    }
-
-    public static <KeyType, ValueType> void garbageCollect(History<KeyType, ValueType> history) {
-        ArrayList<Transaction<KeyType, ValueType>> txns = history.getTransactions();
-        HashSet<Transaction<KeyType, ValueType>> remain = new HashSet<>(txns.size() / 3 * 4 + 1);
-        remain.add(txns.get(0));
-        for (ArrayList<Transaction<KeyType, ValueType>> writeToKeyTxns : history.getKeyWritten().values()) {
-            remain.add(writeToKeyTxns.get(writeToKeyTxns.size() - 1));
-        }
-        for (ArrayList<Transaction<KeyType, ValueType>> writeToKeyTxns : history.getKeyWritten().values()) {
-            for (int j = 1; j < writeToKeyTxns.size() - 1; j++) {
-                Transaction<KeyType, ValueType> txn = writeToKeyTxns.get(j);
-                if (!remain.contains(txn)) {
-                    writeToKeyTxns.remove(j);
-                    j--;
-                    txns.remove(txn);
-                    txn = null;
-                }
-            }
-        }
-        remain = null;
-        System.gc();
     }
 }
